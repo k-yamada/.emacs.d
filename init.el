77fc4c706a40c1ef544da2299c62455579525556
Kazuhiro Yamada <kyamada@sonix.asia>
2013-02-08 18:27:05 +0900

    Remove ruby-encoding-map

--- init.el
+++ init.el
@@ -199,7 +199,7 @@
 (autoload 'run-ruby "inf-ruby" "Run an inferior Ruby process")
 (autoload 'inf-ruby-keys "inf-ruby" "Set local key defs for inf-ruby in ruby-mode")
 (add-hook 'ruby-mode-hook '(lambda () (inf-ruby-keys)))
-(add-to-list 'ruby-encoding-map '(utf-8-hfs . utf-8))
+;;(add-to-list 'ruby-encoding-map '(utf-8))
 
 ;; ruby-electric
 (require 'ruby-electric)
