086c615c3c327c1e6e0632c3d70ca648253b1891
Kazuhiro Yamada <kyamada@sonix.asia>
2013-09-07 23:36:28 +0900

    Use uniquify

--- init.el
+++ init.el
@@ -7,6 +7,16 @@
 ;; 基本設定
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+;; ファイル名がかぶった場合に、バッファ名をわかりやすくする
+;;------------------
+(require 'uniquify)
+;; filename<dir> 形式のバッファ名にする
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+;; *で囲まれたバッファ名は対象外にする
+(setq uniquify-ignore-buffers-re "*[^*]+*")
+
+
 ;; 文字化け対策
 (setq locale-coding-system 'utf-8)
 
