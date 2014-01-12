a49f4b6162dc1cd2fa679c71d7167ef996578523
k-yamada <yamadakazu45@gmail.com>
2014-01-12 21:56:55 +0900

    Commentout highlight

--- init.el
+++ init.el
@@ -639,25 +639,25 @@
 
 
 ;;; C系統,Ruby,Pythonにて1行80文字を超えるとハイライト
-(add-hook 'c-mode-hook
-  (lambda ()
-    (font-lock-add-keywords nil
-      '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
-(add-hook 'c++-mode-hook
-  (lambda ()
-    (font-lock-add-keywords nil
-      '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
-(add-hook 'python-mode-hook
-  (lambda ()
-    (font-lock-add-keywords nil
-      '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
-(add-hook 'ruby-mode-hook
-  (lambda ()
-    (font-lock-add-keywords nil
-      '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
-
-;;; Javaで1行100文字を超えるとハイライト
-(add-hook 'java-mode-hook
-  (lambda ()
-    (font-lock-add-keywords nil
-      '(("^[^\n]\\{100\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
+;;(add-hook 'c-mode-hook
+;;  (lambda ()
+;;    (font-lock-add-keywords nil
+;;      '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
+;;(add-hook 'c++-mode-hook
+;;  (lambda ()
+;;    (font-lock-add-keywords nil
+;;      '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
+;;(add-hook 'python-mode-hook
+;;  (lambda ()
+;;    (font-lock-add-keywords nil
+;;      '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
+;;(add-hook 'ruby-mode-hook
+;;  (lambda ()
+;;    (font-lock-add-keywords nil
+;;      '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
+;;
+;;;;; Javaで1行100文字を超えるとハイライト
+;;(add-hook 'java-mode-hook
+;;  (lambda ()
+;;    (font-lock-add-keywords nil
+;;      '(("^[^\n]\\{100\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
