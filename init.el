5073912c7adfe91dda9fa2f0850cd3e8c8cbf79d
k-yamada <yamadakazu45@gmail.com>
2014-01-16 01:06:40 +0900

    Install smart-compile.el

--- init.el
+++ init.el
@@ -435,6 +435,12 @@
 (when (executable-find "git")
   (require 'egg nil t))
 
+;; smart-compile
+;; (install-elisp "http://www.emacswiki.org/emacs/download/smart-compile.el")
+(require 'smart-compile)
+(define-key ruby-mode-map (kbd "C-c c") 'smart-compile)
+(define-key ruby-mode-map (kbd "C-c C-c") (kbd "C-c c C-m"))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; キーバインド
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
